9d69630c0ab1f408f02922e1e688ac918a1ae980
KC9955 <76588322+KC9955@users.noreply.github.com>
2022-09-15 12:34:54 +0530

    Add files via upload

---
+++ src/Notes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>RED SAUCE PASTA</h1>
+      <hr></hr>
+      <p>
+      Indian cuisine is known for its versatility and also for its adapting nature from other cuisines. 
+      especially it has adapted myriad recipes from western and eurpoean cuisine.
+      one such easy and simple adapted recipe is the traditional pasta recipe or popularly known as 
+      red sauce pasta recipe.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
